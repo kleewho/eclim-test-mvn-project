86d45a3b8e6b1281a41f97f90f63dd742c1f3e26
Lukasz Klich <lukasz.klich@upc.pl>
2015-06-22 07:33:14 +0200

    Reproducing issue #178
    [#178](https://github.com/senny/emacs-eclim/issues/178)

--- lib-a/src/main/java/com/acme/liba/LibraryA.java
+++ lib-a/src/main/java/com/acme/liba/LibraryA.java
@@ -1,5 +1,12 @@
 package com.acme.liba;
 
 public class LibraryA {
+
+    public LibraryA() {}
+
     public static String STATIC_STRING_LIB_A = "Static string from lib a";
+
+    public String getter() {
+        return "String from getter";
+    }
 }
--- proj/src/main/java/com/acme/proj/Project.java
+++ proj/src/main/java/com/acme/proj/Project.java
@@ -3,9 +3,19 @@ package com.acme.proj;
 import static com.acme.liba.LibraryA.STATIC_STRING_LIB_A;
 import static com.acme.libb.LibraryB.STATIC_STRING_LIB_B;
 
+import com.acme.liba.LibraryA;
+
 public class Project {
     public static void main(String[] args) {
         System.out.println("This string is from liba: " + STATIC_STRING_LIB_A);
         System.out.println("This string is from libb: " + STATIC_STRING_LIB_B);
+
+        LibraryA liba = new LibraryA();
+
+        liba.getter();
+
+        //issue #178 - company problems
+        //try to complete something like this: liba.getter()
+        //liba.
     }
 }
